1998749d2b84afad8727bafd22cb229670bcbbeb
nathanbridgeearney <80077609+nathanbridgeearney@users.noreply.github.com>
2021-05-07 17:46:44 +1200

    e

--- SIRSimulation/Events.java
+++ SIRSimulation/Events.java
@@ -1,24 +1,31 @@
 import ecs100.UI;
 
+import javax.swing.*;
 import java.awt.*;
 
 public class Events {
     public double x, y;
     public int size, radius, duration;
+    Color eventColor = new Color(255, 255, 0, 159);
+    Location eventLocation;
 
-    public Events(double x, double y, int size, int radius, int duration){
+
+    public Events(double x, double y, int size, int radius, int duration) {
         this.x = x;
         this.y = y;
         this.size = size;
         this.radius = radius;
         this.duration = duration;
+        eventLocation = new Location(x, y);
     }
 
-    public void draw(){
-        UI.setColor(Color.blue);
-        UI.fillOval(x-radius,y-radius,radius,radius);
-    }
+    public void draw() {
+        UI.setColor(eventColor);
+        UI.fillOval(x - radius, y - radius, radius * 2, radius * 2);
+        duration--;
 
+    }
 
 }
+
 
--- SIRSimulation/SIRSimulation.java
+++ SIRSimulation/SIRSimulation.java
@@ -17,39 +17,39 @@ import java.awt.Color;
 /**
  * SIRSimulation
  * An individual-based simulation program for the SIR (Susceptible-Infected-Recovered) model for spread of diseases.
- * The program creates a list of individuals each with some location within a defined region.
- * The individuals move around in some random way, and may come in contact with each other.
+ * The program creates a list of Individual each with some location within a defined region.
+ * The Individual move around in some random way, and may come in contact with each other.
  * An individual may be susceptible to the disease, infected, or recovered.
- * The simulation should start with all the individuals in the Susceptible state.
+ * The simulation should start with all the Individual in the Susceptible state.
  * If an infected individual comes in contact with a susceptible individual, then with some probability,
  * the susceptible individual may become infected.
  * After some period of time, an infected individual will recover.
  * The simulation should run as a loop with each cycle of the loop representing one time step,
- * in which each individual moves, and may change their state, and then the whole set of individuals
+ * in which each individual moves, and may change their state, and then the whole set of Individual
  * are redrawn in their new positions and new states.
  * The program should let the user control the speed of the simulation (by changing the delay between time steps).
  * There are several numeric parameters controlling the simulation which the user should be able to
  * modify (eg, using sliders). The parameters include at least the following:
  * - the population size
- * - the distance between individuals at which they are considered "in contact"
+ * - the distance between Individual at which they are considered "in contact"
  * - the probability that a susceptible individual will be infected if they are in contact
  * with an infected individual.
  * - the time that an infected individual takes to recover
- * The program should allow the user to place new infected individuals into the simulation (using the mouse).
+ * The program should allow the user to place new infected Individual into the simulation (using the mouse).
  * The program should also allow the user to place "Events" in the simulation (using the mouse)
- * An event will attract all the individuals in its "attraction radius" to move to the event.
- * The individuals will stay at the event until the event is stopped; then they will leave in random directions.
+ * An event will attract all the Individual in its "attraction radius" to move to the event.
+ * The Individual will stay at the event until the event is stopped; then they will leave in random directions.
  * The program should also allow the user to place quarantine regions in the simulation.
- * A quarantine region should be a rectangular region that individuals are not allowed to exit.
+ * A quarantine region should be a rectangular region that Individual are not allowed to exit.
  * (They may or may not be prevented from entering them).
  * The user should be able to add multiple quarantine regions, and should be able to get rid of them.
  * <p>
- * There are lots of different possible movement behaviours of the individuals.
+ * There are lots of different possible movement behaviours of the Individual.
  * The more realistic the behaviour,  the more complicated it is to implement.
- * Some kind of random motion, where individuals move in the same direction
+ * Some kind of random motion, where Individual move in the same direction
  * for some number of steps, then change to a different random direction is OK (except for
  * events, where they should head straight for the event).
- * It would be nice to have individuals moving mostly between home, work, and the shops,
+ * It would be nice to have Individual moving mostly between home, work, and the shops,
  * with random other trips, but this is much more complicated.
  */
 public class SIRSimulation {
@@ -57,11 +57,11 @@ public class SIRSimulation {
 
     // Constants
     public static final Region BOUNDARY = new Region(10, 10, 900, 600);
-    private static final ArrayList<Individuals> citizens = new ArrayList<>();
-    public static final ArrayList<Region> quarantines = new ArrayList<>();
-    public static final ArrayList<Events> events = new ArrayList<>();
+    private static ArrayList<Individual> citizens = new ArrayList<>();
+    public static ArrayList<Region> quarantines = new ArrayList<>();
+    public static ArrayList<Events> events = new ArrayList<>();
 
-    public static ArrayList<Individuals> getCitizens() {
+    public static ArrayList<Individual> getCitizens() {
         return citizens;
     }
 
@@ -77,11 +77,13 @@ public class SIRSimulation {
     public static double infectRad;
     public static double infectProb;
     public static int infectTimer = 100;
+    public static int eventDuration;
 
     public static int getInfectionTime() {
         return infectTimer;
     }
 
+    boolean eventRunning = false;
     int stepTimer = 250 / 5;
     private static Location startLoc;
     boolean running = false;
@@ -104,7 +106,7 @@ public class SIRSimulation {
         UI.clearGraphics();
         citizens.clear();
         for (int i = 0; i < popNum; i++) {
-            citizens.add(new Individuals());
+            citizens.add(new Individual());
         }
         while (running = true) {
             stepCount++;
@@ -114,13 +116,22 @@ public class SIRSimulation {
             UI.clearGraphics();
             BOUNDARY.draw(Color.black);
             for (Region r : quarantines) r.draw(Color.BLACK);
-            for (Individuals i : citizens) {
-                i.move();
-                i.step();
+            for (int i = 0; i < citizens.size(); i++) {
+                Individual iF = citizens.get(i);
+                iF.move();
+                iF.step();
             }
-            for (Events e : events){
-                e.draw();
+            for (int e = 0; e < events.size(); e++) {
+                Events eF = events.get(e);
+                if (eF.duration == 0) {
+                    eventRunning = false;
+                    events.remove(e);
+                } else {
+                    eventRunning = true;
+                    eF.draw();
+                }
             }
+
             UI.repaintGraphics();
             UI.sleep(stepTimer);
         }
@@ -137,7 +148,7 @@ public class SIRSimulation {
 
     public void doMouse(String action, double x, double y) {
         if (action.equals("pressed") && mAction == 'I') {
-            citizens.add(new Individuals(x, y));
+            citizens.add(new Individual(x, y));
         } else if (mAction == 'Q') {
             if (action.equals(("pressed"))) {
                 startLoc = new Location(x, y);
@@ -148,9 +159,10 @@ public class SIRSimulation {
         } else if (mAction == 'E') {
             if (action.equals(("pressed"))) {
                 startLoc = new Location(x, y);
-            }else if (action.equals("released")){
+                eventDur(100);
+            } else if (action.equals("released")) {
                 int rad = (int) (x - startLoc.getX());
-                events.add(new Events(startLoc.getX(), startLoc.getY(), 100, rad, 100));
+                events.add(new Events(startLoc.getX(), startLoc.getY(), 100, rad, eventDuration));
 
             }
         }
@@ -159,6 +171,11 @@ public class SIRSimulation {
 
     private void removeQuarantine() {
         quarantines.clear();
+        quarantines.add(new Region(10, 10, 900, 600));
+    }
+
+    public void eventDur(double v) {
+        eventDuration = (int) v;
     }
 
     private void infectionProbability(double v) {
@@ -177,10 +194,11 @@ public class SIRSimulation {
 
     public void setupGUI() {
 
-        UI.addSlider("Population Size", 0, 1000, 0, this::popCount);
+        UI.addSlider("Population Size", 0, 1000, 1, this::popCount);
         UI.addButton("Reset", this::reset);
         UI.addButton("Add Infected", () -> mAction = 'I');
         UI.addButton("Event", () -> mAction = 'E');
+        UI.addSlider("Event Duration", 0, 1000, 100, this::eventDur);
         UI.addButton("Quarantine", () -> mAction = 'Q');
         UI.addButton("Remove Quarantine", this::removeQuarantine);
         UI.addSlider("Infection Probability:", 0, 100, 0, this::infectionProbability);
